9745b5c015cc5ee6644837048a9beb8f2c5c5ee1
Gustavo Dinizm <gustavodnz035@gmail.com>
2021-09-10 14:45:00 -0300

    Seeds added, Invoice model created, controllers created

---
+++ backend/database/seeders/InvoiceSeed.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Invoice;
+use Illuminate\Database\Seeder;
+
+class InvoiceSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Invoice::truncate();
+
+        $invoices = array(
+            array(
+                'user_id' => 1,
+                'product_id' => 1,
+                'product_description' => 'Nvidia Gtx 1080',
+                'price' => 600.99
+            ),
+            array(
+                'user_id' => 1,
+                'product_id' => 2,
+                'product_description' => 'Nvidia Gtx 1070',
+                'price' => 500.99
+            ),
+            array(
+                'user_id' => 2,
+                'product_id' => 3,
+                'product_description' => 'ASUS GeForce RTX 2060',
+                'price' => 749.99
+            ),
+            array(
+                'user_id' => 2,
+                'product_id' => 4,
+                'product_description' => 'GIGABYTE Radeon RX 6700 XT GAMING OC',
+                'price' => 1040.99
+            ),
+            array(
+                'user_id' => 3,
+                'product_id' => 5,
+                'product_description' => 'GIGABYTE GeForce GTX 1650 SUPER WINDFORCE',
+                'price' => 517
+            ),
+            array(
+                'user_id' => 3,
+                'product_id' => 6,
+                'product_description' => 'ASUS ROG STRIX Radeon RX 6700 XT OC',
+                'price' => 1199.99
+            ),            
+        );
+
+        Invoice::insert($invoices);
+    }
+}
---
+++ backend/database/seeders/UserSeed.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $users = array(
+            array(
+                'name' => 'test-1',
+                'email' => 'test1@test.com',
+                'password' => Hash::make(Str::random(10))
+            ),
+            array(
+                'name' => 'test-2',
+                'email' => 'test2@test.com',
+                'password' => Hash::make(Str::random(10))
+            ),
+            array(
+                'name' => 'test-3',
+                'email' => 'test3@test.com',
+                'password' => Hash::make(Str::random(10))
+            )
+        );
+
+        User::insert($users);
+    }
+}
